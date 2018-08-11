b1275c77238c628107a41e23cb8d88176f20ca90
pranav7491 <sathepranav77@gmail.com>
2018-08-11 17:17:00 -0500

    Update

--- Houseprice.py
+++ Houseprice.py
@@ -9,6 +9,9 @@ from scipy import stats
 import warnings
 warnings.filterwarnings('ignore')
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+
+
 %matplotlib inline
 
 train = pd.read_csv("C:/Users/prana/Desktop/Data competition/Salepricetrain.csv")
@@ -94,5 +97,159 @@ train.shape
 #Electrical variable has only one missing value so missing value record is deleted and the variable could be used.
 
 
+#Bivariate Analysis
 
- 
+var = "GrLivArea"
+data = pd.concat([train["SalePrice"],train[var]], axis=1)
+data.plot.scatter(x=var,y="SalePrice",ylim=(0,800000))
+
+df = train[(train.GrLivArea < 4675) & (train.SalePrice < 700000)]
+
+print(df["GrLivArea"])
+
+#Removed outliers from GrLivArea
+var = "GrLivArea"
+data = pd.concat([df["SalePrice"],df[var]], axis=1)
+data.plot.scatter(x=var,y="SalePrice",ylim=(0,800000), figsize=(14,10))
+
+
+fig = plt.figure(figsize=(14, 10))
+sns.distplot(df["SalePrice"], fit=norm)
+fig = plt.figure(figsize=(14, 10))
+res = stats.probplot(df['SalePrice'], plot=plt)
+
+#Log transformation
+df['SalePrice']=np.log(df['SalePrice'])
+
+fig = plt.figure(figsize=(14, 10))
+sns.distplot(df["SalePrice"], fit=norm)
+fig = plt.figure(figsize=(14, 10))
+res = stats.probplot(df['SalePrice'], plot=plt)
+
+
+
+
+##########
+
+train_corr = train.select_dtypes(include=[np.number])
+
+
+corr = train_corr.corr()
+plt.subplots(figsize=(20,9))
+sns.heatmap(corr, annot=True)
+
+
+top_feature = corr.index[abs(corr['SalePrice']>0.5)]
+plt.subplots(figsize=(12, 8))
+top_corr = train[top_feature].corr()
+sns.heatmap(top_corr, annot=True)
+plt.show()
+
+
+train.OverallQual.unique()
+
+sns.barplot(train.OverallQual, train.SalePrice)
+
+
+
+col = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'TotRmsAbvGrd', 'YearBuilt']
+sns.set(style='ticks')
+sns.pairplot(train[col], size=3, kind='reg')
+
+#Missing value imputation
+train['PoolQC'] = train['PoolQC'].fillna('None')
+
+train['MiscFeature'] = train['MiscFeature'].fillna('None')
+train['Alley'] = train['Alley'].fillna('None')
+train['Fence'] = train['Fence'].fillna('None')
+train['FireplaceQu'] = train['FireplaceQu'].fillna('None')
+
+train['LotFrontage'] = train.groupby("Neighborhood")["LotFrontage"].transform(
+    lambda x: x.fillna(x.median()))
+
+for col in ['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond']:
+    train[col] = train[col].fillna('None')
+for col in ('BsmtFinType2', 'BsmtExposure', 'BsmtFinType1', 'BsmtCond', 'BsmtQual'):
+    train[col] = train[col].fillna('None')
+
+
+
+
+for col in ['GarageYrBlt', 'GarageArea', 'GarageCars']:
+    train[col] = train[col].fillna(int(0))
+train['MasVnrArea'] = train['MasVnrArea'].fillna(int(0))    
+
+train['MasVnrType'] = train['MasVnrType'].fillna('None')
+
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond', 
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1', 
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond', 
+        'YrSold', 'MoSold', 'MSZoning', 'LandContour', 'LotConfig', 'Neighborhood',
+        'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st',
+        'Exterior2nd', 'MasVnrType', 'MasVnrArea', 'Foundation', 'GarageType', 'MiscFeature', 
+        'SaleType', 'SaleCondition', 'Electrical', 'Heating')
+
+
+
+
+train['Electrical'] = train['Electrical'].fillna(train['Electrical']).mode()[0]
+
+train = train.drop(['Utilities'], axis=1)
+
+plt.figure(figsize=(10, 5))
+sns.heatmap(train.isnull())
+
+#Data Transformation
+for c in cols:
+    lbl = LabelEncoder() 
+    lbl.fit(list(train[c].values)) 
+    train[c] = lbl.transform(list(train[c].values))
+    
+y = train['SalePrice']
+
+del train['SalePrice']
+
+##### Linear Regression
+from sklearn import linear_model
+model = linear_model.LinearRegression()
+
+X = train.values
+y = y.values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
+
+model.fit(X_train, y_train)
+
+print("Predict value " + str(model.predict([X_test[200]])))
+print("Real value " + str(y_test[200]))
+
+print("Accuracy --> ", model.score(X_test, y_test)*100)
+
+#Accuracy -->  86.2665070745
+
+
+###### RandomForestRegression
+
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor(n_estimators=1000)
+
+model.fit(X_train, y_train)
+
+print("Accuracy --> ", model.score(X_test, y_test)*100)
+
+#Accuracy -->  90.46438384
+
+######  GradientBoosting Regression
+
+from sklearn.ensemble import GradientBoostingRegressor
+GBR = GradientBoostingRegressor(n_estimators=100, max_depth=4)
+
+GBR.fit(X_train, y_train)
+
+print("Accuracy --> ", GBR.score(X_test, y_test)*100)
+
+
+#Accuracy -->  92.1761126819
--- ReadMe.txt
+++ ReadMe.txt
@@ -5,11 +5,34 @@ which could be used predict the price accurately to help a business make informe
 Decisions.
 
 So far,
+
 - I have plotted Living area and Sale price. The plot indicates Living area has significant linear relation with Sale price.
+
 - The overall quality of house is directly proportional with Sale price.
+
 - The Total Basement area of a house has more of a quadratic relation between Sale price. After 2000 Square Feet Sale price increase is qudratic.
+
 - For YearBuilt the Sale Price is linear initially but, it increases qudratically after the year 1960.
+
 - The HeatMap Feature is used to find Correlation between various numerical attributes and, to decide whether to use them or to drop them. 
-- The pairplot diagram gives us the relation between variables. Variables TotalBsmtSF,  GrLiveArea have a partila linear relation. Also, variables SalePrice and YearBuilt have a rough exponential relation.
 
+- I have compared dependent variable SalePrice for linearity asuumption. I also tried to plot SalePrice to compare its distribution with normal distribution where, I have realized the data is right skewed.
+Therefore, I performed log transformation to convert SalePrice to a Normal distribution.
 
+- I have selected highly correlated attributes with more than 0.5 correlation. I have also tried to draw a pairplot of highly correlated attributes.
+
+- Attributes with more than 0.5 corraltion with SalePrice are taken off from the model.
+
+-While imputing missing values, attributes with more than 50% of missing values are imputed with None.
+
+- Lotfrontage missing values are imputed by neighbourhood lot frontage values.
+
+- Categorical Garage mssing values are imputed with None, while numerical with 0.
+
+- In the final step, I have selected variables with less than 0.5 correlation to build the final model.
+
+=> Linear Regression model is giving an accuracy of 86.2665070745 %.
+
+=> RandomForest Regression is giving an accurancy of 90.46438384 %.
+
+=> GradientBoosting Regression is giving an accuracy of 92.1761126819 %. 
